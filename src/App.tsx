31094c6fd18277960f4341b82ad5604fbaa9b130
e-doschechnikova <evadrobakova@gmail.com>
2022-06-10 15:04:47 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Accordion from "./components/Accordion/Accordion";
+import Rating from "./components/Rating/Rating";
+
+function App(props: any) {
+  console.log("App rendering");
+  return (
+    <div className="App">
+      <PageTitle title={"This is APP component"} />
+      <PageTitle title={"My friends"} />
+      Artikle 1
+      <Rating value={0} />
+      <Accordion titleValue={"Menu"} />
+      <Accordion titleValue={"Users"} />
+      Artikle 2
+      <Rating value={1} />
+      <Rating value={2} />
+      <Rating value={3} />
+      <Rating value={4} />
+      <Rating value={5} />
+    </div>
+  );
+}
+
+type PageTitlePropsType = {
+  title: string;
+};
+
+function PageTitle(props: PageTitlePropsType) {
+  console.log("PageTitle rendering");
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  );
+}
+
+export default App;
