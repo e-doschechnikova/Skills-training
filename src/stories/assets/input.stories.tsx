3d9647e9190abcfc17028234efa307398950e3d5
e-doschechnikova <evadrobakova@gmail.com>
2022-07-03 12:03:59 +0300

    add ControlledSelect

--- src/stories/assets/input.stories.tsx
+++ src/stories/assets/input.stories.tsx
@@ -54,4 +54,19 @@ export const ControlledCheckbox = () => {
   return <input type="checkbox" checked={parentValue} onChange={onChange} />;
 };
 
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>("2");
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setParentValue(e.currentTarget.value);
+  };
+  return (
+    <select value={parentValue} onChange={onChange}>
+      <option>none</option>
+      <option value={"1"}>Minsk</option>
+      <option value={"2"}>Moscow</option>
+      <option value={"3"}>Helsinki</option>
+    </select>
+  );
+};
+
 export const ControlledInputWithFixedValue = () => <input value={"hello"} />;
